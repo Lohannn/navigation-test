fdb593ad12c1797e9e2d67a24aa076832d877d3d
Lohannn <lohannes.s26@gmail.com>
2023-08-04 16:30:16 -0300

    Navigation Completo e animações

--- app/src/main/java/br/senai/sp/jandira/myapplication/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/myapplication/MainActivity.kt
@@ -3,23 +3,31 @@ package br.senai.sp.jandira.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.shrinkHorizontally
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+//import androidx.navigation.compose.composable
 import br.senai.sp.jandira.myapplication.login.screen.LoginScreen
 import br.senai.sp.jandira.myapplication.menu.screen.MenuScreen
 import br.senai.sp.jandira.myapplication.pedido.screen.PedidoScreen
 import br.senai.sp.jandira.myapplication.perfil.screen.PerfilScreen
 import br.senai.sp.jandira.myapplication.ui.theme.MyApplicationTheme
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
+import com.google.accompanist.navigation.animation.composable
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -29,10 +37,24 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val navController = rememberNavController()
-                    NavHost(
+                    val navController = rememberAnimatedNavController()
+                    AnimatedNavHost(
                         navController = navController,
                         startDestination = "login",
+                        exitTransition = {
+                            slideOutOfContainer(
+                                AnimatedContentScope.SlideDirection.Up,
+                                tween(3000)
+                            ) +
+                            fadeOut(tween(600))
+                        },
+                        enterTransition = {
+                            slideIntoContainer(
+                                AnimatedContentScope.SlideDirection.Down,
+                                tween(350)
+                            ) +
+                                    fadeIn(tween(600))
+                        }
                     ) {
                         composable(route = "login") { LoginScreen(navController) }
                         composable(route = "menu") { MenuScreen(navController) }
